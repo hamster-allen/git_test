4fb7c36754ef22d6276d67c5009ca47fa7c137d0
allen <okbom926@gmail.com>
2024-08-02 01:49:13 +0800

    8/2

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+
+
+int main(void)
+{
+  for(int i=0; i<10; i++)
+  {
+    if(i == 5)
+    {
+      printf("hi");
+    }
+    else
+    {
+      
+    }
+  }
+}
+
+
+
---
+++ main_py.py
@@ -0,0 +1,11 @@
+
+for i in range(0, 10):
+  if(i == 5):
+    print("hi")
+
+
+
+
+
+
+
