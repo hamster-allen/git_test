3ebdf38280dc4628df24f79209b79822f573a6df
allen <okbom926@gmail.com>
2024-08-02 02:58:18 +0800

    python test1

--- main_py.py
+++ main_py.py
@@ -3,18 +3,3 @@ for i in range(0, 10):
   if(i == 5):
     print("hi")
 
-for z in range(10, 0, -1):
-  if(i == 5):
-    print("hi")
-
-for z in range(10, 0, -1):
-  if(i == 5):
-    print("hi")
-
-for z in range(10, 0, -1):
-  if(i == 5):
-    print("hi")
-
-for z in range(10, 0, -1):
-  if(i == 5):
-    print("hi")
